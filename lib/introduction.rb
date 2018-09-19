965d3bbda714e78f5b41b2942d178ae51280139c
Virginia Dooley <vcdooley@gmail.com>
2018-09-19 09:22:18 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  introduction + 'and I am learning to programe in #{language}.'
+  "Hi, my name is #{name} and I am learning to programe in #{language}."
 end
 
 puts introduction_with_language("Dan", "Ember.js")
