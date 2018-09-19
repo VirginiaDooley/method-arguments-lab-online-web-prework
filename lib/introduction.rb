a336cbfaa6657804c91f3a981b205a96e653ce11
Virginia Dooley <vcdooley@gmail.com>
2018-09-19 09:21:44 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-
 def introduction_with_language(name, language)
   introduction + 'and I am learning to programe in #{language}.'
 end
